9e06ded402a00decca27fffa37f64a13929e87d0
Andreas From <andro.from@gmail.com>
2014-09-13 22:17:32 +0200

    give this precedence code a stab more, this time taking more properly associativity into account

--- Form.hs
+++ Form.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Form where
+module Form (showForm, Form(..), Assignments, Formula, (+++)) where
 
 import qualified Data.Foldable as F
 import qualified Data.Text     as T
@@ -18,7 +18,7 @@ data Form a = Atom a
             | Xor (Form a) (Form a)
             | If (Form a) (Form a)
             | Iff (Form a) (Form a)
-            deriving (Functor, F.Foldable, Eq, Ord)
+            deriving (Functor, F.Foldable)
 
 instance Show (Formula) where
   show = T.unpack . showForm
@@ -31,15 +31,39 @@ showForm :: Formula -> T.Text
 showForm (Atom p) = p
 showForm (Not (Atom p)) = "¬" +++ p
 showForm (Not p) = "¬(" +++ showForm p +++ ")"
-showForm o@(And p q) = precedence o p "∧" q
-showForm o@(Or p q)  = precedence o p "∨" q
-showForm o@(Xor p q) = precedence o p "⊕" q
-showForm o@(If p q)  = precedence o p " ⇒ " q
-showForm o@(Iff p q) = precedence o p " ⇔ " q
+showForm o@(And p q) = parens (grade o) (grade p) "∧" (grade q)
+showForm o@(Or p q)  = parens (grade o) (grade p) "∨" (grade q)
+showForm o@(Xor p q) = parens (grade o) (grade p) "⊕" (grade q)
+showForm o@(If p q)  = parens (grade o) (grade p) " ⇒ " (grade q)
+showForm o@(Iff p q) = parens (grade o) (grade p) " ⇔ " (grade q)
 
-precedence :: Formula -> Formula -> T.Text -> Formula -> T.Text
-precedence o p op q
-  | p <= o && q <= o = showForm p +++ op +++ showForm q
-  | q <= o = "(" +++ showForm p +++ ")" +++ op +++ showForm q
-  | p <= o = showForm p +++ op +++ "(" +++ showForm q +++ ")"
-  | otherwise = "(" +++ showForm p +++ ")" +++ op +++ "(" +++ showForm q +++ ")"
+data Assoc = Associative | AssocLeft | AssocRight
+           deriving (Show, Eq)
+
+data Precedence a = Precedence (Form a) Int Int Assoc
+
+grade :: Form a -> Precedence a
+grade p@(Atom _)  = Precedence p 0 0 Associative
+grade p@(Not _)   = Precedence p 1 1 Associative
+grade p@(And _ _) = Precedence p 2 2 Associative
+grade p@(Or _ _)  = Precedence p 2 3 Associative
+grade p@(Xor _ _) = Precedence p 2 4 Associative
+grade p@(If _ _)  = Precedence p 3 5 AssocRight
+grade p@(Iff _ _) = Precedence p 3 6 Associative
+
+parens :: Precedence T.Text -> Precedence T.Text -> T.Text -> Precedence T.Text -> T.Text
+parens (Precedence _ ol ot oa) (Precedence p pl pt _) op (Precedence q ql qt _)
+  | ot == pt && oa == Associative = (if ql < ol then plains else rights) p op q
+  | ot == qt && oa == Associative = (if pl < ol then plains else lefts) p op q
+  | ot == pt && ql < ol && oa == AssocLeft = lefts p op q
+  | ot == qt && pl < ol && oa == AssocRight = rights p op q
+  | pl < ol && ql < ol = plains p op q
+  | pl < ol = rights p op q
+  | ql < ol = lefts p op q
+  | otherwise = boths p op q
+
+plains, lefts, rights, boths :: Formula -> T.Text -> Formula -> T.Text
+plains p op q = showForm p +++ op +++ showForm q
+lefts p op q = "(" +++ showForm p +++ ")" +++ op +++ showForm q
+rights p op q = showForm p +++ op +++ "(" +++ showForm q +++ ")"
+boths p op q = "(" +++ showForm p +++ ")" +++ op +++ "(" +++ showForm q +++ ")"
--- README.md
+++ README.md
@@ -12,24 +12,6 @@ It's easiest to just show the functionality with a picture:
 
 I hope to generate some binaries some time soon.
 
-# Conventions
-
-I've adopted a strict precedence hierarchy for the logical connectives to simplify parsing and printing.
-
-The operators are presented here from highest to lowest precedence:
-
-- Negation: ¬
-- Conjunction: ∧
-- Disjunction: ∨
-- Exclusive disjunction: ⊕
-- Implication: ⇒
-- Biconditional: ⇔
-
-It must be noted that implication is right associative so `p ⇒ q ⇒ r` is parsed as `p ⇒ (q ⇒ r)`.
-The rest are associative.
-
-To give a (contrived) example, the proposition: `¬p∧q v r ⇔ q` is parsed as `(((¬p)∧q) v r) ⇔ q`
-
 # Code Notes
 
 The parser, expression evaluation and half of the truth table logic is very nice code.
