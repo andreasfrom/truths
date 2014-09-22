1412268fdcc529051e0d84afd995bce8f23c73cb
Andreas From <andro.from@gmail.com>
2014-09-22 21:07:05 +0200

    build a proper truthtable datatype to give better output and clean up buildTable and equivalent in the process

--- Main.hs
+++ Main.hs
@@ -12,6 +12,17 @@ import           Parser
 import           System.Console.Readline
 import           System.Environment      (getArgs)
 
+newtype TruthTable = TruthTable [[T.Text]]
+
+instance Show TruthTable where
+  show = T.unpack . showTable
+
+showTable :: TruthTable -> T.Text
+showTable (TruthTable (header:rows)) = T.intercalate "\n" (T.intercalate "\t" header : (map go rows))
+  where go :: [T.Text] -> T.Text
+        go row = T.concat (zipWith (\col t -> col +++ T.concat (replicate t "\t")) row tabs)
+        tabs = map (succ . (`div` 8) . T.length) header
+
 assign :: Form T.Text -> Assignments -> Form Bool
 assign form as = fmap (fromJust . flip lookup as) form
 
@@ -35,35 +46,27 @@ table form = zip rows results
   where rows = cases (atoms form)
         results = map truth (map (assign form) rows)
 
-prettyTable :: T.Text -> T.Text -> Form T.Text -> T.Text
-prettyTable t f form = T.intercalate "\t" (atoms form) +++ "\t" +++ showForm form +++ "\n" +++ go (table form)
-  where go [] = ""
-        go ((as,b):rest) = format as +++ showBool t f b +++ "\n" +++ go rest
-        format [] = ""
-        format ((_,p):bs) = showBool t f p +++ "\t" +++ format bs
+buildTable :: T.Text -> T.Text -> Form T.Text -> TruthTable
+buildTable t f form = TruthTable $ (atoms form ++ [showForm form]) : foldl go [] (table form)
+  where go acc (as,b) = (foldl (\ps (_,p) -> showBool t f p : ps) [] as ++ [showBool t f b]) : acc
 
 showBool :: T.Text -> T.Text -> Bool -> T.Text
 showBool t _ True = t
 showBool _ f False = f
 
--- Needs cleaning up
 equivalent :: T.Text -> T.Text -> Formula -> Formula -> Either T.Text ()
-equivalent tr fa f g | length f' /= length g' =
-              Left "the propositions contain a different number of atoms"
-          | not (null zipped) =
-              Left ("the propositions differ in the case(s) below:\n"
-                    +++ T.intercalate "\t" (zipWith (\p q -> if p==q then p else p +++ "|" +++ q) (atoms f) (atoms g))
-                    +++ "\t" +++ showForm f +++ "\t" +++ showForm g
-                    +++ "\n" +++ format (filter (not . snd) zipped))
-          | otherwise = Right ()
-  where zipper (as,b) (as',c) = (((as,b), (as',c)), b == c)
-        f' = table f
-        g' = table g
-        zipped = filter (not . snd) $ zipWith zipper f' g'
-        format [] = ""
-        format ((((fs,fb), (_,gb)),_):rest) = T.intercalate "\t" (map (showBool tr fa . snd) fs) +++ "\t"
-                                               +++ showBool tr fa fb +++ "\t" +++ showBool tr fa gb
-                                               +++ "\n" +++ format rest
+equivalent tr fa f g
+  | length (table f) /= length (table g) = Left "the propositions contain a different number of atoms"
+  | not (null different) =
+      Left $ "the propositions differ in the cases below:\n"
+      +++ showTable (TruthTable ([zipWith name (atoms f) (atoms g) ++ [showForm f] ++ [showForm g]]
+                                 ++ map (format . fst) different))
+  | otherwise = Right ()
+  where different = filter (not . snd) $ zipWith differ (table f) (table g)
+        differ (as,b) (_,c) = ((as,b,c), b == c)
+        format (fs,fb,gb) = (map (showBool tr fa . snd) fs) ++ (map (showBool tr fa) [fb,gb])
+        name a b | a == b = a
+        name a b = a +++ "/" +++ b
 
 -- Needs cleaning up, could use the Reader monad for T F/1 0-configuration
 main :: IO ()
@@ -77,10 +80,10 @@ main = putStrLn "Enter a proposition (or \"exit\"):" >> getArgs >>= fltr >>= loo
              addHistory line >>
              case (parseInput (T.pack line)) of
               Right prop' -> case prop' of
-                              Single p -> (putStrLn $ T.unpack $ prettyTable t f p) >> loop (t, f)
-                              Equivalence p q -> (case (equivalent t f p q) of
-                                               Left e -> putStrLn (T.unpack e)
-                                               Right () -> putStrLn (T.unpack t)) >> loop (t, f)
+                              Single p -> (putStrLn $ show $ buildTable t f p) >> loop (t, f)
+                              Equivalence p q -> (case equivalent t f p q of
+                                                   Left e -> putStrLn $ T.unpack e
+                                                   Right () -> putStrLn $ T.unpack t) >> loop (t, f)
               Left e -> print e >> loop (t, f)
 
         fltr [x,y] = return (T.pack x, T.pack y)
