73d0a209fdee4bbba8a07669c49764a36f5d1831
Andreas From <andro.from@gmail.com>
2014-09-22 21:18:25 +0200

    clean up main and the handling of user-defined T/F-representations a bit

--- Main.hs
+++ Main.hs
@@ -2,14 +2,14 @@
 
 module Main where
 
-import           Control.Monad           (replicateM)
+import           Control.Monad           (forever, replicateM)
 import qualified Data.Foldable           as F
 import           Data.List               (nub)
 import           Data.Maybe              (fromJust)
 import qualified Data.Text               as T
 import           Form
 import           Parser
-import           System.Console.Readline
+import           System.Console.Readline (addHistory, readline)
 import           System.Environment      (getArgs)
 
 newtype TruthTable = TruthTable [[T.Text]]
@@ -46,16 +46,17 @@ table form = zip rows results
   where rows = cases (atoms form)
         results = map truth (map (assign form) rows)
 
-buildTable :: T.Text -> T.Text -> Form T.Text -> TruthTable
-buildTable t f form = TruthTable $ (atoms form ++ [showForm form]) : foldl go [] (table form)
-  where go acc (as,b) = (foldl (\ps (_,p) -> showBool t f p : ps) [] as ++ [showBool t f b]) : acc
+buildTable :: ShowBool -> Form T.Text -> TruthTable
+buildTable showB form = TruthTable $ (atoms form ++ [showForm form]) : foldl go [] (table form)
+  where go acc (as,b) = (foldl (\ps (_,p) -> showB p : ps) [] as ++ [showB b]) : acc
 
-showBool :: T.Text -> T.Text -> Bool -> T.Text
+type ShowBool = Bool -> T.Text
+showBool :: T.Text -> T.Text -> ShowBool
 showBool t _ True = t
 showBool _ f False = f
 
-equivalent :: T.Text -> T.Text -> Formula -> Formula -> Either T.Text ()
-equivalent tr fa f g
+equivalent :: ShowBool -> Formula -> Formula -> Either T.Text ()
+equivalent showB f g
   | length (table f) /= length (table g) = Left "the propositions contain a different number of atoms"
   | not (null different) =
       Left $ "the propositions differ in the cases below:\n"
@@ -64,27 +65,25 @@ equivalent tr fa f g
   | otherwise = Right ()
   where different = filter (not . snd) $ zipWith differ (table f) (table g)
         differ (as,b) (_,c) = ((as,b,c), b == c)
-        format (fs,fb,gb) = (map (showBool tr fa . snd) fs) ++ (map (showBool tr fa) [fb,gb])
+        format (fs,fb,gb) = (map (showB . snd) fs) ++ (map showB [fb,gb])
         name a b | a == b = a
         name a b = a +++ "/" +++ b
 
--- Needs cleaning up, could use the Reader monad for T F/1 0-configuration
 main :: IO ()
-main = putStrLn "Enter a proposition (or \"exit\"):" >> getArgs >>= fltr >>= loop
-  where loop (t, f) = do
-          prop <- readline "> "
-          case prop of
-           Nothing -> return ()
-           Just "exit" -> return ()
-           Just line ->
-             addHistory line >>
-             case (parseInput (T.pack line)) of
-              Right prop' -> case prop' of
-                              Single p -> (putStrLn $ show $ buildTable t f p) >> loop (t, f)
-                              Equivalence p q -> (case equivalent t f p q of
-                                                   Left e -> putStrLn $ T.unpack e
-                                                   Right () -> putStrLn $ T.unpack t) >> loop (t, f)
-              Left e -> print e >> loop (t, f)
+main = putStrLn "Enter a proposition (or \"exit\"):" >> getArgs >>= fltr >>= \(t,f) -> forever $ do
+  prop <- readline "> "
+  case prop of
+   Nothing -> return ()
+   Just "exit" -> return ()
+   Just line ->
+     addHistory line >>
+     case (parseInput (T.pack line)) of
+      Left e -> print e
+      Right prop' -> case prop' of
+        Single p -> (putStrLn $ show $ buildTable (showBool t f) p)
+        Equivalence p q -> case equivalent (showBool t f) p q of
+          Left e -> putStrLn $ T.unpack e
+          Right () -> putStrLn $ T.unpack t
 
-        fltr [x,y] = return (T.pack x, T.pack y)
+  where fltr [x,y] = return (T.pack x, T.pack y)
         fltr _ = return ("T", "F")
